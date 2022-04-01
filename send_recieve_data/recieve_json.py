6d7b902664ecfad080faa3bdc02c71bc6dae5a47
kent <autonomafordon@gmail.com>
2022-04-01 14:44:20 +0200

    recieving json possible recieve_json.py

--- send_recieve_data/recieve_json.py
+++ send_recieve_data/recieve_json.py
@@ -1,6 +1,7 @@
 import socket
+import json
 
-UDP_IP = "192.168.0.4"
+UDP_IP = "192.168.0.2"
 UDP_PORT = 5005
 
 
@@ -10,4 +11,6 @@ sock.bind((UDP_IP, UDP_PORT))
 
 while True:
 	data, addr = sock.recvfrom(1024) # buffer size is 1024 byte
+	data = data.decode("utf-8")
+	data = json.loads(data)
 	print(data["name"]+" är "+data["age"])
