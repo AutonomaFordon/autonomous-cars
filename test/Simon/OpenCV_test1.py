46eadf9259497080df81e864e9695e68dba4609e
kent <autonomafordon@gmail.com>
2022-04-04 22:26:10 +0200

    Further testing of line detection, work in progress

--- test/Simon/OpenCV_test1.py
+++ test/Simon/OpenCV_test1.py
@@ -3,45 +3,64 @@ import cv2 #OpenCV for video processing
 import numpy as np
 import math
 import time
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 
-#Setup video feed
-cap = cv2.VideoCapture(0) #Create videocapture object
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1600) #Set video stream width
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 900) #Set video stream height
-cap.set(cv2.CAP_PROP_FPS, 90) #Set max frame rate to 5fps
-time.sleep(1) #Let the camera init to light conditions etc
-if not cap.isOpened(): #Check if camera feed is ok
-    print("Cannot open camera")
-    exit()
+#Setup camera
+camera = PiCamera()
+camera.resolution = (640, 480) #Set resolution
+camera.framerate = 32 #Set framerate
+rawCapture = PiRGBArray(camera, size=(640, 480))
 
+#Give camera time to warmup
+time.sleep(1)
+
+# Set up streem
+stream = camera.capture_continuous(rawCapture, format="bgr", use_video_port=True, resize=(640, 480))
 
 while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
+    image = next(stream) #Grab new frame
+    frame = image.array #Convert to array for processing in opencv
+    rawCapture.truncate(0) #Clear streem buffer 
+    
+    #Convert to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #Convert to grayscale
+    gray = cv2.GaussianBlur(gray,(5, 5),0)
+
     
     #Canny edge detection
-    edges = cv2.Canny(frame,50,51, None, 3)
+    edges = cv2.Canny(frame,50,150, None, 3)
     
-    #Hough line detection
-    linesP = cv2.HoughLinesP(edges, 1, np.pi / 180, 50, None, 50, 10)
+    mask = np.zeros_like(edges)
+    ignore_mask_color = 255
     
-    if linesP is not None:
-        for i in range(0, len(linesP)):
-            l = linesP[i][0]
-            cv2.line(frame, (l[0], l[1]), (l[2], l[3]), (0,0,255), 2, cv2.LINE_AA)
+    imshape = frame.shape
+    vertices = np.array([[(0,imshape[0]),(100, 160), (540, 160), (imshape[1],imshape[0])]], dtype=np.int32)
+    cv2.fillPoly(mask, vertices, ignore_mask_color)
+    masked_edges = cv2.bitwise_and(edges, mask)
 
-    cv2.imshow("Detected Lines (in red) - Probabilistic Line Transform", frame)
+    rho = 2 # distance resolution in pixels of the Hough grid
+    theta = np.pi/180 # angular resolution in radians of the Hough grid
+    threshold = 15     # minimum number of votes (intersections in Hough grid cell)
+    min_line_length = 40 #minimum number of pixels making up a line
+    max_line_gap = 30    # maximum gap in pixels between connectable line segments
+    line_image = np.copy(frame)*0 # creating a blank to draw lines on
+
+    lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]),min_line_length, max_line_gap)
+
+    if not lines.lengthl:
+        print("No lines found. Exiting...")
+        break
     
-    # Display the resulting frame
-    #cv2.imshow('Video output', edges)
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
+
+    cv2.imshow("Result", line_image)
+    
+    
     if cv2.waitKey(1) == ord('q'): #Break if q is pressed
         break
 
 #Clean and free resources
-cap.release()
 cv2.destroyAllWindows()
