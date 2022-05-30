54e7c92718316decd002fba20c71d3c8949bfeb5
AutonomaFordon <102529681+AutonomaFordon@users.noreply.github.com>
2022-05-30 14:37:01 +0200

    Update send_recieve.py

--- main/car/send_recieve.py
+++ main/car/send_recieve.py
@@ -6,21 +6,24 @@ from protocols import *
 
 conf_key="Ghent2021!"
 
+# connect to the server
 def socket_connect():
     global host
     global port
     global sock
-    host = "192.168.0.3"
+    host = "192.168.0.5"
     port = 5005
     sock = socket.socket()
     try:
+        # Connect
         sock.connect((host, port))
+        # Recieve a establish_conn protocol, this part is a handshake
+        # between the server and the device
         answer = sock.recvfrom(1024)
-        #answer = answer.decode("utf-8")
-        print(str(answer))
-        answer = json.dumps(answer)
-        answer = convert_json_protocol(answer.__dict__)
-        sock.send(bytes(conf_key))
+        answer = answer.decode("utf-8")
+        #print(str(answer))
+        answer = json.loads(answer)
+        sock.send(conf_key)
     except socket.error as msg:
         print("Error trying to connect to socket: "+str(msg))
         print("Retrying...")
