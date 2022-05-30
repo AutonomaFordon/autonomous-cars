817a8798f0c90459eb4c5c128e9556a84a6a6ff0
Kasimda <Simon.benstorp@gmail.com>
2022-05-30 21:03:30 +0200

    Added comments

--- main/car/LaneDetection.py
+++ main/car/LaneDetection.py
@@ -1,48 +1,51 @@
-import cv2 #Open CV
-import numpy as np #Numpy
+import cv2  # Open CV
+import numpy as np  # Numpy
 
 import time
 
-import utlis #Supporting functions
- 
-#Global vars 
-curveList = [] #List of latest curve values
-avgVal=10
+import utlis  # Supporting functions
 
-def getLaneCurve(img,display=2): #Takes an image as input and calculates the curve (direction of the lane)
-                                 #img is the input image and display determines if we should viualize the substeps (set to 0 to disable and 1 to enable)
- 
-    imgCopy = img.copy() #Create copy of img
-    imgResult = img.copy() #Create second copy of img
-    
-    #Convert image to binary, so that everyhing is black except of the road markings
+# Global vars
+curveList = []  # List of latest curve values
+avgVal = 10  # Set max length of curveList
+
+
+def getLaneCurve(img, display=2):  # Takes an image as input and calculates the curve (direction of the lane)
+                                   # img is the input image and display determines if we should visualize the sub steps (set to 0 to disable and 1 to enable)
+
+    imgCopy = img.copy()  # Create copy of img
+    imgResult = img.copy()  # Create second copy of img
+
+    # Convert image to binary, so that everything is black except of the road markings
     imgThres = utlis.thresholding(img)
- 
-    #Warap the image so it resembles what it would look like from above
-    hT, wT, c = img.shape #Extract img hight width and channels to variables
-    points = utlis.valTrackbars() #Grab reference points to warp image
-    imgWarp = utlis.warpImg(imgThres,points,wT,hT) #Preform the warp
-    imgWarpPoints = utlis.drawPoints(imgCopy,points) #Visualize the reference points
-    
-    #Erode the img, removes some noise
-    countours = cv2.erode(imgWarp,(3,3),iterations = 1)
-    
-    #Detect the drivable area from the processed img, retuned imgage/array has drivable pixels set 1 and all other set to 0
+
+    # Warap the image so it resembles what it would look like from above
+    hT, wT, c = img.shape  # Extract img hight width and channels to variables
+    points = utlis.valTrackbars()  # Grab reference points to warp image
+    imgWarp = utlis.warpImg(imgThres, points, wT, hT)  # Preform the warp
+    imgWarpPoints = utlis.drawPoints(imgCopy, points)  # Visualize the reference points
+
+    # Erode the img, removes some noise
+    countours = cv2.erode(imgWarp, (3, 3), iterations=1)
+
+    # Detect the drivable area from the processed img, retuned imgage/array has drivable pixels set 1 and all other set to 0
     driveable_area = utlis.detect_lane_area(countours)
-     
-    #Calculate midpoint of drivalbe area using a historam
-    middlePoint,imgHist = utlis.getHistogram(driveable_area,display=True,minPer=0.5,region=4) #Calcualte midoint of line directly in front of car
-    curveAveragePoint, imgHist = utlis.getHistogram(driveable_area, display=True, minPer=0.9) #Calculate midpoint of lane area 
-    curveRaw = curveAveragePoint - middlePoint
- 
-    #### SETP 4
-    curveList.append(curveRaw)
-    if len(curveList)>avgVal:
+
+    # Calculate midpoint of drivalbe area using a historam
+    middlePoint, imgHist = utlis.getHistogram(driveable_area, display=True, minPer=0.5,
+                                              region=4)  # Calcualte midoint of lane directly in front of car
+    curveAveragePoint, imgHist = utlis.getHistogram(driveable_area, display=True,
+                                                    minPer=0.9)  # Calculate midpoint of lane area
+    curveRaw = curveAveragePoint - middlePoint  # Calculate curve value be taking difference between points
+
+    curveList.append(curveRaw)  # Store result in list
+    if len(curveList) > avgVal:  # Clear 1 element if list is to long
         curveList.pop(0)
-    curve = int(sum(curveList)/len(curveList))
- 
-    #### STEP 5
+    curve = int(sum(curveList) / len(curveList))  # Take average value
+
+    # Finaly, visualize the steps if display is not set to 0
     if display != 0:
+        # The following comands processes the images from some of the substeps and puts them besieds eachother in 3x2 grid for visualization
         imgInvWarp = utlis.warpImg(driveable_area, points, wT, hT, inv=True)
         imgInvWarp = cv2.cvtColor(imgInvWarp, cv2.COLOR_GRAY2BGR)
         imgInvWarp[0:hT // 3, 0:wT] = 0, 0, 0
@@ -58,23 +61,22 @@ def getLaneCurve(img,display=2): #Takes an image as input and calculates the cur
             w = wT // 20
             cv2.line(imgResult, (w * x + int(curve // 50), midY - 10),
                      (w * x + int(curve // 50), midY + 10), (0, 0, 255), 2)
-        #fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
-        #cv2.putText(imgResult, 'FPS ' + str(int(fps)), (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (230, 50, 50), 3);
+
     if display == 2:
         imgStacked = utlis.stackImages(0.7, ([img, imgWarpPoints, driveable_area],
                                              [imgHist, imgLaneColor, imgResult]))
         cv2.imshow('ImageStack', imgStacked)
-    elif display == 1:
-        cv2.imshow('Resutlt', imgResult)
+    ##We previously had a second debug option for display == 1 but it has been removed
 
+    # Normalize the curve value to stay in relative close proximity to 0 (max of 1 and min of -1)
+    curve = curve / 100
+    if curve > 1: curve == 1
+    if curve < -1: curve == -1
 
-    #### NORMALIZATION
-    curve = curve/100
-    if curve>1: curve ==1
-    if curve<-1:curve == -1
- 
     return curve
- 
 
-intialTrackBarVals = [3, 18, 0, 60 ]
-utlis.initializeTrackbars(intialTrackBarVals)
+
+# Initial setup
+intialTrackBarVals = [3, 18, 0, 60]  # Set default values for the trackbar which are used to warp the image
+utlis.initializeTrackbars(intialTrackBarVals)  # Add the default values to the trackbars
+
--- main/car/utlis.py
+++ main/car/utlis.py
@@ -1,19 +1,19 @@
 import cv2 #Open CV
 import numpy as np #Numpy
 
-#Convert image to binary, so that everyhing is black except of the road markings
+#Convert image to binary, so that everything is black except of the road markings
 def thresholding(img):
     imgHsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV) # converts image values to HSV
     lowerWhite = np.array([0,40,70]) #lower bound
     upperWhite = np.array([50,255,255]) #higher bound
-    maskWhite = cv2.inRange(imgHsv,lowerWhite,upperWhite) #if x camerapixel is within the lower and higher bound, x pixel will be white, 
+    maskWhite = cv2.inRange(imgHsv,lowerWhite,upperWhite) #if x camerapixel is within the lower and higher bound, x pixel will be white,
     #if not, x pixel will be black
     return maskWhite
 
 #Waraps the image so it resembles what it would look like from above
 def warpImg(img,points,w,h,inv = False):
     pts1 = np.float32(points) #Copy if input img/array
-    pts2 = np.float32([[0,0],[w,0],[0,h],[w,h]]) #Create new/empty array with supplied dimensions 
+    pts2 = np.float32([[0,0],[w,0],[0,h],[w,h]]) #Create new/empty array with supplied dimensions
     if inv: #If inverted
         matrix = cv2.getPerspectiveTransform(pts2, pts1) #Get inverted matrix
     else:
@@ -30,7 +30,7 @@ def initializeTrackbars(intialTracbarVals,wT=120, hT=60):
     cv2.createTrackbar("Width Bottom", "Trackbars", intialTracbarVals[2],wT//2, nothing)
     cv2.createTrackbar("Height Bottom", "Trackbars", intialTracbarVals[3], hT, nothing)
 
-    
+#Function that grabs the values from the trackbars and return them as a array
 def valTrackbars(wT=120, hT=60):
     widthTop = cv2.getTrackbarPos("Width Top", "Trackbars")
     heightTop = cv2.getTrackbarPos("Height Top", "Trackbars")
@@ -39,36 +39,38 @@ def valTrackbars(wT=120, hT=60):
     points = np.float32([(widthTop, heightTop), (wT-widthTop, heightTop),
                       (widthBottom , heightBottom ), (wT-widthBottom, heightBottom)])
     return points
- 
+
+#Function that based on a set of points draw a circle
 def drawPoints(img,points):
     for x in range(4):
         cv2.circle(img,(int(points[x][0]),int(points[x][1])),15,(0,0,255),cv2.FILLED)
     return img
- 
+
+#Sums all pixels in every column and calculates the average points x coordinate from the sums
 def getHistogram(img,minPer=0.1,display= False,region=1):
- 
-    if region ==1:
-        histValues = np.sum(img, axis=0)
-    else:
-        histValues = np.sum(img[img.shape[0]//region:,:], axis=0)
- 
-    #print(histValues)
-    maxValue = np.max(histValues)
-    minValue = minPer*maxValue
- 
-    indexArray = np.where(histValues >= minValue)
-    basePoint = int(np.average(indexArray))
-    #print(basePoint)
- 
+
+    if region ==1: #If region 1 one
+        histValues = np.sum(img, axis=0) #Sum all pixels
+    else: #Else
+        histValues = np.sum(img[img.shape[0]//region:,:], axis=0) #Sum the pixels in lover part of the image
+
+    maxValue = np.max(histValues) #Calc max value
+    minValue = minPer*maxValue #Create minimum value filter based on max value and parameter
+
+    indexArray = np.where(histValues >= minValue) #Filter out to small values
+    basePoint = int(np.average(indexArray)) #Create avg point from remaining values
+
+    #Create a visualization image if display is set to 1
     if display:
         imgHist = np.zeros((img.shape[0],img.shape[1],3),np.uint8)
         for x,intensity in enumerate(histValues):
             cv2.line(imgHist,(x,img.shape[0]),(x,img.shape[0]-intensity//255//region),(255,0,255),1)
             cv2.circle(imgHist,(basePoint,img.shape[0]),20,(0,255,255),cv2.FILLED)
         return basePoint,imgHist
- 
+
     return basePoint
- 
+
+#Stacks the 6 (in our case) images in a 3x2 grid
 def stackImages(scale,imgArray):
     rows = len(imgArray)
     cols = len(imgArray[0])
@@ -96,37 +98,35 @@ def stackImages(scale,imgArray):
             else:
                 imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
             if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        hor= np.hstack(imgArray)
+        hor = np.hstack(imgArray)
         ver = hor
     return ver
 
+#Detects the drivable area on a binary image free of noise
 def detect_lane_area(img):
-    
-    #cv2.imshow("Test:", img)
-    
-    height, width = img.shape
-    driveable_area = np.zeros(img.shape, np.uint8)
-    
-    points = []
-    
-    for row in range(0, height, 10):
-        pixel_l = int(width/2)
+
+    height, width = img.shape #Get dimensions
+    driveable_area = np.zeros(img.shape, np.uint8) # Create blank area to draw the drivable area on
+    points = [] #Array to store points that define the borders of the drivable area
+
+    for row in range(0, height, 10): #For every 10th row in the input image
+        pixel_l = int(width/2) #Find/reset midpoint
+
+        #Walk "step by step" from middle to the left until we find the road markings/a white pixel
+        #If we don’t find any road markings we add assume all area to the left at that point is drivable
         while img[row][pixel_l] == 0 and pixel_l != 0:
             pixel_l -= 1
-        #Add coordinate
-        #points = np.append(points, [row, pixel_l])
+        #When we find the border add coordinate where we found it to points array
         points.append([pixel_l, row])
-        
+
+        #Repeat process but this time rom middle to the right
         pixel_r = int(width/2)
         while img[row][pixel_r] == 0 and pixel_r != width-1:
             pixel_r += 1
-        #Add c
-        #points = np.append(arr=points, values=[row, pixel_r])
+        #Add cordinate
         points.insert(0,[pixel_r, row])
-    
-    points2 = np.copy(points)
-    cv2.fillPoly(driveable_area, [points2], color=(255))
+
+    points2 = np.copy(points) #Create a new array with correct datatype
+    cv2.fillPoly(driveable_area, [points2], color=(255)) #Fill the driveable area using fillPoly and the found edges on the drivable area
     return driveable_area
-
-
 
