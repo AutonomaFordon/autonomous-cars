99c914aa4d020fa64d23ec7a09f6f8bd97d802bd
AutonomaFordon <102529681+AutonomaFordon@users.noreply.github.com>
2022-05-30 14:07:43 +0200

    Added comments

--- main/motorFunctionsParallell.py
+++ main/motorFunctionsParallell.py
@@ -7,14 +7,16 @@ speed_r = 0 #Right motor speed
 
 def setup(pins = [[13,5],[12,6]], read_speed_pins=[20,16]):
     
-    #Establishing pins for motors
-    global motorl
+    #Declare global variables
+    global motorl 
     global motorr
+    
+    #Establishing pins for motors
     motorl = pins[0]
     motorr = pins[1]
     
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM) #Set label type on GPIO pins to GPIO-pin numbers
+    GPIO.setwarnings(False) #Disable warning msg in console
 
     #Set all motor pins as output
     GPIO.setup(motorl[0], GPIO.OUT)
@@ -32,7 +34,7 @@ def setup(pins = [[13,5],[12,6]], read_speed_pins=[20,16]):
     pwml.start(0)
     pwmr.start(0)
 
-    #all motor pins = 0
+    #all motor pins set to zero
     GPIO.output(motorl[0], False)
     GPIO.output(motorl[1], False)
     GPIO.output(motorr[0], False)
@@ -43,47 +45,55 @@ def setup(pins = [[13,5],[12,6]], read_speed_pins=[20,16]):
 
 #Function used to set speed from main
 def setSpeed(left=0, right=0): 
-    global speed_l #Define 
+    #Declare global variables
+    global speed_l
     global speed_r
     
+    #Update global variables
     speed_l = left
     speed_r = right
 
     
-def trackSpeed():
+def trackSpeed(): #Continuously tracks the motors speed and adjust to keep at set value global speed value
+    #Declare global variables
     global speed_l
     global speed_r
     
-    max_time = 0.2
+    max_time = 0.2 #Max time before timeout, timeout results in speed as
     
+    #Set start value for PWM-signal
     pwm_l = 50
     pwm_r = 50
     
     while(True):
-        if(speed_r==0):
+        if(speed_r==0): #If right speed is 0 - turn of left right motor
             pwmr.ChangeDutyCycle(0)
-        else:
-            pwm_r = upadateSpeed(pwm_r, speed.read_speed(values=2,motor=1,time_lim=max_time)[1], speed_r)
-            pwmr.ChangeDutyCycle(pwm_r)
+        else: 
+            pwm_r = upadateSpeed(pwm_r, speed.read_speed(values=2,motor=1,time_lim=max_time)[1], speed_r) #Read current speed and increase/decrease pwm depending on if current speed is greater or less than set speed
+            pwmr.ChangeDutyCycle(pwm_r) #Update motor setting to new PWM-signal
             
             
-        if(speed_l==0):
+        if(speed_l==0): #If left speed is 0 - turn of left left motor
             pwml.ChangeDutyCycle(0)
         else:
-            pwm_l = upadateSpeed(pwm_l, speed.read_speed(values=2,motor=0,time_lim=max_time)[1], speed_l)
-            pwml.ChangeDutyCycle(pwm_l)
-        
-        #print("PWM_R =" + str(pwm_r) + "      PWM_L = " + str(pwm_l))
+            pwm_l = upadateSpeed(pwm_l, speed.read_speed(values=2,motor=0,time_lim=max_time)[1], speed_l) #Read current speed and increase/decrease pwm depending on if current speed is greater or less than set speed
+            pwml.ChangeDutyCycle(pwm_l) #Update motor setting to new PWM-signal
             
             
-def upadateSpeed(pwm_in, speed_in, target_speed):
+def upadateSpeed(pwm_in, speed_in, target_speed): #Updates the pwm
     if(speed_in==0):
-        pwm_out = pwm_in + 15
+        pwm_out = pwm_in + 15   # if the wheel isn't turning it needs a high
+                                # PWM-signal to start and should therefor increase
+                                # more than if it is moving
     else:
+        # if there is a large differense between target and current speed a larger
+        # change is needed. The change in the PWM-signal is determined using the
+        # difference between target och current speed and multiplying it by 0.1 
         pwm_out = pwm_in + (target_speed - speed_in)*0.1
-     
+
+    # The PWM signal has to be within the interval: 0 <= PWM <= 100
     if(pwm_out<0):
-        pwm_out = 0
+        pwm_out = 0 
     if(pwm_out>100):
         pwm_out=100
 
